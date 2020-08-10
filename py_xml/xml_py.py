7325f809b4b9f67ab7250cf4b396eb4b7a577990
rodd <dodolangus@gmail.com>
2020-08-10 21:02:05 +0700

    testing

--- py_xml/xml_py.py
+++ py_xml/xml_py.py
@@ -1,3 +1,32 @@
-print "hello"
+import pandas as pd
+import numpy as np
+import xml.etree.ElementTree as ET
 
-print "iya"
+path_xml = "E:/corpu/"
+
+class xml_mail():
+
+    def __init__(self):
+        self.value = 'XML'
+
+
+
+    def get_xml():
+        file_xml = "report_realisasi_sharing_2018.xml"
+        tree = ET.parse(path_xml+file_xml)
+        root = tree.getroot()
+        return root
+
+
+
+
+# main = xml_mail()
+
+if __name__ == '__main__':
+    print(xml_mail.get_xml())
+
+
+
+
+
+
